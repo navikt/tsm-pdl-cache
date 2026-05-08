0391f7023f045c0b7e923315d4870cd3fef42da4
Andreas <danduras@gmail.com>
2026-05-08 16:09:11 +0200

    chore: give zara access

--- src/main/kotlin/no/nav/tsm/pdl/cache/pdl/PdlNameUpdateConsumer.kt
+++ src/main/kotlin/no/nav/tsm/pdl/cache/pdl/PdlNameUpdateConsumer.kt
@@ -2,6 +2,7 @@ package no.nav.tsm.pdl.cache.pdl
 
 import com.fasterxml.jackson.module.kotlin.readValue
 import io.opentelemetry.instrumentation.annotations.WithSpan
+import no.nav.tsm.pdl.cache.person.PersonService
 import no.nav.tsm.pdl.cache.util.objectMapper
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.LoggerFactory
@@ -9,12 +10,12 @@ import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
 
 @Service
-class PdlNameUpdateConsumer(val pdlPersonService: PdlPersonService) {
+class PdlNameUpdateConsumer(val pdlPersonService: PdlPersonService, val personService: PersonService) {
 
     private val logger = LoggerFactory.getLogger(PdlNameUpdateConsumer::class.java)
 
     @WithSpan
-    @KafkaListener(id = "tsm-pdl-cache-consumer-name-udpate-2", topics = ["pdl.pdl-persondokument-v1"])
+    @KafkaListener(id = "tsm-pdl-cache-consumer-name-udpate-3", topics = ["pdl.pdl-persondokument-v1"])
     fun consume(record: ConsumerRecord<String, String?>) {
 
         val pdlPerson =  record.value()?.let { objectMapper.readValue<PdlPerson>(it) }
@@ -28,20 +29,19 @@ class PdlNameUpdateConsumer(val pdlPersonService: PdlPersonService) {
         }
 
         logger.info("updating name for person with aktorid ${record.key()}")
-        val persons = pdlPersonService.personRepository.getPerson(record.key())
-        when (persons.size) {
-            0 -> logger.info("Person with aktorid ${record.key()} not found")
-            1 -> {
-                logger.info("Person with aktorid ${record.key()} found, updating")
-                val person = persons.single()
-                if(person.navn == null) {
-                    pdlPersonService.personRepository.updateName(record.key(), getName(pdlPerson) ?: throw IllegalStateException("must have a name"))
-                } else {
-                    logger.info("Person with aktorid ${record.key()} has name no need to update")
-                }
-            }
-            else -> logger.info("Person with aktorid ${record.key()} found ${persons.size} persons, not updating")
-            }
+        val person = personService.getPerson(record.key())
 
+        if(person.identer.firstOrNull { !it.historisk && it.gruppe == IDENT_GRUPPE.AKTORID && it.ident == record.key()} == null) {
+            logger.warn("person with aktorId ${record.key()} is not currently active")
+            return
+        }
+
+
+        logger.info("Person with aktorid ${record.key()} found, updating")
+        if(person.navn == null ) {
+            pdlPersonService.personRepository.updateName(record.key(), getName(pdlPerson) ?: throw IllegalStateException("must have a name"))
+        } else {
+            logger.info("Person with aktorid ${record.key()} has name no need to update")
+        }
     }
 }
