a05b4d40593e20938f56b18bc2a71be1e620f766
Karl J. Overå <k@rl.run>
2026-07-28 10:00:14 +0200

    chore: add simple integration test from consumer to db to API

--- src/test/kotlin/no/nav/tsm/pdl/cache/testutils/Integration.kt
+++ src/test/kotlin/no/nav/tsm/pdl/cache/testutils/Integration.kt
@@ -1,5 +1,18 @@
 package no.nav.tsm.pdl.cache.testutils
 
+import java.util.Properties
+import kotlin.collections.set
+import kotlin.io.use
+import kotlin.use
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.apache.kafka.clients.admin.AdminClient
+import org.apache.kafka.clients.admin.AdminClientConfig
+import org.apache.kafka.clients.admin.NewTopic
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.ByteArraySerializer
+import org.apache.kafka.common.serialization.StringSerializer
 import org.flywaydb.core.Flyway
 import org.jetbrains.exposed.v1.jdbc.Database
 import org.testcontainers.kafka.ConfluentKafkaContainer
@@ -41,6 +54,43 @@ abstract class WithPostgresql {
 
 abstract class WithPostgresAndKafka : WithPostgresql() {
     companion object {
-        val kafka = ConfluentKafkaContainer("confluentinc/cp-kafka:8.1.0").apply { start() }
+        private val topics = listOf("pdl.pdl-persondokument-v1")
+
+        val kafka =
+            ConfluentKafkaContainer("confluentinc/cp-kafka:8.1.0").apply {
+                start()
+                createTopics()
+            }
+
+        private fun ConfluentKafkaContainer.createTopics() {
+            val props =
+                Properties().apply {
+                    this[AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServers
+                }
+            AdminClient.create(props).use { admin ->
+                admin.createTopics(topics.map { NewTopic(it, 1, 1) }).all().get()
+            }
+        }
+
+        fun recreateTopics() {
+            val props =
+                Properties().apply {
+                    this[AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG] = kafka.bootstrapServers
+                }
+
+            AdminClient.create(props).use { admin ->
+                admin.deleteTopics(topics).all().get()
+                admin.createTopics(topics.map { NewTopic(it, 1, 1) }).all().get()
+            }
+        }
+    }
+
+    suspend fun produce(topic: String, key: String, value: ByteArray?) {
+        withContext(Dispatchers.IO) {
+            val props = Properties().apply { this["bootstrap.servers"] = kafka.bootstrapServers }
+            KafkaProducer(props, StringSerializer(), ByteArraySerializer()).use { producer ->
+                producer.send(ProducerRecord(topic, key, value)).get()
+            }
+        }
     }
 }
