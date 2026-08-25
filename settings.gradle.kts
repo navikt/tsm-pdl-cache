16cbd9b846bb5c12d99954649a6837f7e817210e
Karl J. Overå <k@rl.run>
2026-08-25 16:15:14 +0200

    automated: upgrade tsm-ktor libs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ rootProject.name = "tsm-pdl-cache"
 include(":client")
 
 val ktorVersion = "3.5.2"
-val tsmKtorVersion = "1.2.13"
+val tsmKtorVersion = "1.3.0"
 
 dependencyResolutionManagement {
     repositories {
